d20376819c7bbac402e0f18fba34398d0908ba89
Leonhard Driesch <leo.driesch@gmail.com>
2021-03-09 09:23:35 +0100

    :wrench: Fix storybook setup with emotion

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,23 @@
+const path = require('path');
+
+const toPath = (_path) => path.join(process.cwd(), _path);
+
+// https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
 module.exports = {
-    stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
+    stories: ['../**/*.stories.@(ts|tsx|js|jsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-    // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
-    typescript: {
-        check: true, // type-check stories during Storybook build
+    webpackFinal: async (config) => {
+        return {
+            ...config,
+            resolve: {
+                ...config.resolve,
+                alias: {
+                    ...config.resolve.alias,
+                    '@emotion/core': toPath('node_modules/@emotion/react'),
+                    '@emotion/styled': toPath('node_modules/@emotion/styled'),
+                    'emotion-theming': toPath('node_modules/@emotion/react'),
+                },
+            },
+        };
     },
 };
